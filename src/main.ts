0374e138fa40a2a9f422e2576586ea20858f1e8d
unknown <jeanned'arc@cchub.rw>
2020-03-16 11:09:39 +0200

    user authentication

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('Nestjs auth example')
+    .setDescription('Project API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
