03b7a450951abd261664341a7e7edcde0f0f7cb1
unknown <jeanned'arc@cchub.rw>
2020-03-16 14:07:11 +0200

    deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
-    MongooseModule.forRoot('mongodb://localhost/nestauth', {
+    MongooseModule.forRoot(process.env.DB_HOST, {
       useNewUrlParser: true,
     }),
   ],
